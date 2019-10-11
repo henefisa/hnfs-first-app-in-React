fc3c1971a50c815eabb2fe5767e8b44a4a4fc939
Henefisa <cherishnghia@gmail.com>
2019-10-11 11:31:36 +0700

    add more feature for TodoList

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import TrafficLight from "./components/trafficlight";
 import TodoItems from "./components/todoItems";
 import tick from "./svg-icon/tick.svg";
+import classNames from "classnames";
 
 const RED = 0;
 const YELLOW = 1;
@@ -12,13 +13,10 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      filterStatus: null,
       currentColor: RED,
       allComplete: false,
-      todoItems: [
-        { title: "Ngủ", isComplete: false },
-        { title: "Ngáy", isComplete: false },
-        { title: "Say Hola", isComplete: false }
-      ]
+      todoItems: []
     };
     setInterval(() => {
       this.setState({
@@ -28,6 +26,11 @@ class App extends Component {
 
     this.insertTodo = this.insertTodo.bind(this);
     this.completeAll = this.completeAll.bind(this);
+  }
+
+  countTodo() {
+    const { todoItems } = this.state;
+    return todoItems.filter(item => item.isComplete === false).length;
   }
 
   getNextColor(color) {
@@ -48,7 +51,6 @@ class App extends Component {
       const { isComplete } = item;
       const { todoItems } = this.state;
       const index = todoItems.indexOf(item);
-      console.log(index);
       this.setState({
         todoItems: [
           ...todoItems.slice(0, index),
@@ -87,42 +89,116 @@ class App extends Component {
 
   completeAll(event) {
     const { todoItems, allComplete } = this.state;
-    todoItems.forEach((item) => {
+    todoItems.forEach(item => {
       item.isComplete = !allComplete;
     });
     this.setState({
       allComplete: !allComplete
-    })
+    });
+  }
+
+  deleteItem(items) {
+    return event => {
+      const { todoItems } = this.state;
+      let tmpArr = [...todoItems];
+      items.forEach(item => {
+        const index = tmpArr.indexOf(item);
+        tmpArr.splice(index,1);
+      });
+      this.setState({
+        todoItems: tmpArr
+      });
+    };
+  }
+
+  filterTodo(filterValue) {
+    return () => {
+      this.setState({
+        filterStatus: filterValue
+      });
+    };
   }
 
   render() {
     const { currentColor } = this.state;
-    const { todoItems } = this.state;
+    const { todoItems, filterStatus } = this.state;
     return (
       <div className="App">
         <TrafficLight currentColor={currentColor} />
-        <div className="input--todo">
-          <img
-            src={tick}
-            alt="Complete all"
-            width={32}
-            height={32}
-            onClick={this.completeAll}
-          />
-          <input
-            type="text"
-            placeholder="Add a new todo item"
-            onKeyUp={this.insertTodo}
-          />
-        </div>
-        {todoItems.length &&
-          todoItems.map((item, index) => (
-            <TodoItems
-              key={index}
-              item={item}
-              onClick={this.onItemClicked(item)}
+        <div className="todoBox">
+          <div className="input--todo">
+            <img
+              src={tick}
+              alt="Complete all"
+              width={32}
+              height={32}
+              onClick={this.completeAll}
             />
-          ))}
+            <input
+              type="text"
+              placeholder="Add a new todo item"
+              onKeyUp={this.insertTodo}
+            />
+          </div>
+          {todoItems.length > 0 && filterStatus === null
+            ? todoItems.map((item, index) => (
+                <TodoItems
+                  key={index}
+                  item={item}
+                  onClick={this.onItemClicked(item)}
+                  deleteItem={this.deleteItem([item])}
+                />
+              ))
+            : todoItems
+                .filter(item => item.isComplete === filterStatus)
+                .map((item, index) => (
+                  <TodoItems
+                    key={index}
+                    item={item}
+                    onClick={this.onItemClicked(item)}
+                    deleteItem={this.deleteItem([item])}
+                  />
+                ))}
+          {todoItems.length === 0 && (
+            <div className="empty-todoItems">
+              You have no todo items, add a new one
+            </div>
+          )}
+          <div className="bottom-bar">
+            <div className="Counter">
+              {this.countTodo() <= 1
+                ? `${this.countTodo()} item left`
+                : `${this.countTodo()} items left`}
+            </div>
+            <div className="filter-btn">
+              <button
+                className={classNames({
+                  active: filterStatus === null
+                })}
+                onClick={this.filterTodo(null)}
+              >
+                All
+              </button>
+              <button
+                className={classNames({
+                  active: filterStatus === false
+                })}
+                onClick={this.filterTodo(false)}
+              >
+                Active
+              </button>
+              <button
+                className={classNames({
+                  active: filterStatus === true
+                })}
+                onClick={this.filterTodo(true)}
+              >
+                Completed
+              </button>
+            </div>
+            <button onClick={this.deleteItem(this.state.todoItems.filter(item => item.isComplete === true))}>Clear completed.</button>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/todoItems.js
+++ src/components/todoItems.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
-
 import classNames from "classnames";
-
 import "./TodoItems.css";
-
 import check from "../svg-icon/check.svg";
 import checkDone from "../svg-icon/check-complete.svg";
+import deleteIcon from '../svg-icon/cross.svg';
 
 class TodoItems extends Component {
   render() {
-    const { item, onClick } = this.props;
+    const { item, onClick, deleteItem } = this.props;
     let url = check;
     if (item.isComplete) {
       url = checkDone;
@@ -30,6 +28,7 @@ class TodoItems extends Component {
         >
           {item.title}
         </p>
+        <img className="delete-btn" src={deleteIcon} alt="Delete" onClick={deleteItem}/>
       </div>
     );
   }
